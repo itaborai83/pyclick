ae0a170263c2e3fc872bf4f6180d9eee1cbcc124
Daniel Lemos Itaboraí <daniel.itaborai@petrobras.com.br>
2020-04-29 14:04:50 -0300

    created gzip utils to avoid using busybox as much

---
+++ pyclick/gunzip.py
@@ -0,0 +1,41 @@
+# -*- coding: utf8 -*-
+# please set the environment variable PYTHONUTF8=1
+import sys
+import os
+import os.path
+import glob
+import shutil
+import argparse
+import logging
+import datetime as dt
+import pandas as pd
+import sqlite3
+
+import pyclick.util as util
+import pyclick.config as config
+
+assert os.environ[ 'PYTHONUTF8' ] == "1"
+
+logger = util.get_logger('gunzip')
+
+class App(object):
+    
+    VERSION = (0, 0, 0)
+    
+    def __init__(self, filename):
+        self.filename = filename
+    
+    def run(self):
+        try:
+            logger.info('gunzip - version %d.%d.%d', *self.VERSION)
+            util.decompress(self.filename)
+        except:
+            logger.exception('an error has occurred')
+            sys.exit(1)
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str, help='file to be compressed')
+    args = parser.parse_args()
+    app = App(args.filename)
+    app.run()
