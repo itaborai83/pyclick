f88ff454f991aee9a213a8136b7a1c16b5705eba
Daniel Lemos Itaboraí <itaborai83@yahoo.com.br>
2020-05-21 14:53:37 -0300

    script to generate staging files from the replicated db

---
+++ create_stagingv2.bat
@@ -0,0 +1,220 @@
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-01
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-02
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-03
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-04
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-05
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-06
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-07
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-08
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-09
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-10
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-11
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-12
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-13
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-14
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-15
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-16
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-17
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-18
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-19
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-20
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-21
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-22
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-23
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-24
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-25
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-26
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-27
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-28
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-02-29
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-01
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-02
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-03
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-04
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-05
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-06
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-07
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-08
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+rem python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-09
+rem if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-10
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-11
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-12
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-13
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-14
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-15
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-16
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-17
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-18
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-19
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-20
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-21
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-22
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-23
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-24
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-25
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-26
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-27
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-28
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-29
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-30
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-03-31
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-01
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-02
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-03
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-04
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-05
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-06
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-07
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-08
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-09
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-10
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-11
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-12
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-13
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-14
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-15
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-16
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-17
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-18
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-19
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-20
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-21
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-22
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-23
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-24
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-25
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-26
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-27
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-28
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-29
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-04-30
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-01
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-02
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-03
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-04
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-05
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-06
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-07
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-08
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-09
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-10
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-11
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-12
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-13
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-14
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-15
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-16
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-17
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-18
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-19
+if %errorlevel% neq 0 exit /b %errorlevel%
+python -m pyclick.assyst.dump_daily --compress DADOS\STAGINGv2 2020-05-19
+if %errorlevel% neq 0 exit /b %errorlevel%
