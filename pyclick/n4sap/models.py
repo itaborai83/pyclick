fcf1937e9b9633d74fcc5a2c7da4736c03a603fb
Daniel Lemos Itaboraí <itaborai83@yahoo.com.br>
2020-07-28 11:11:38 -0300

    add IncidentService for common computations

--- pyclick/n4sap/models.py
+++ pyclick/n4sap/models.py
@@ -3,8 +3,9 @@ import pyclick.models as models
 
 class ClickN4(models.Click):
     
-    def __init__(self, strict_orientar=False):
+    def __init__(self, incsrv, strict_orientar=False):
         super().__init__()
+        self.incsrv = incsrv
         self.strict_orientar = strict_orientar
         self.expurgos_orientar = set()
     
@@ -24,7 +25,7 @@ class ClickN4(models.Click):
                 return 'ORIENTAR'
             else:
                 return None
-    
+            
     def update(self, event):
         if self.categorizar(event) is None:
             self.expurgos_orientar.add(event.id_chamado)
@@ -53,45 +54,28 @@ class N4SapKpi(models.Kpi):
     ,   False   : 'N'
     ,   None    : None
     }  
-    def __init__(self, logger=None):
+    def __init__(self, incsrv=None, logger=None):
+        if incsrv is None:
+            incsrv = IncidentService()
         if logger is None:
             logger = util.get_null_logger()
         super().__init__()
-        self.override_categorias = {}
+        self.incsrv = incsrv
         self.logger = logger
         
     def set_override_categoria(self, id_chamado, categoria):
-        self.override_categorias[ id_chamado ] = categoria
+        self.incsrv.set_override_categoria(id_chamado, categoria)
     
     def unset_override_categoria(self, id_chamado):
-        del self.override_categorias[ id_chamado ]
+        self.incsrv.unset_override_categoria[ id_chamado ]
     
     def categorizar(self, inc):
-        if inc.id_chamado in self.override_categorias:
-            return self.override_categorias[ inc.id_chamado ]
-        elif inc.id_chamado.startswith('T'):
-            return 'ATENDER - TAREFA'
-        elif inc.id_chamado.startswith('S'):
-            return 'ATENDER'
-        elif 'CORRIGIR' in inc.categoria.upper():
-            return 'CORRIGIR'
-        else:
-            return 'ORIENTAR'
-    
+        return self.incsrv.categorizar(inc)
+        
     def calcular_prazo(self, inc, mesa_atual=None):
         if mesa_atual is None:
-            mesa = inc.mesa_atual
-        categoria = self.categorizar(inc)
-        if mesa == self.MESA_PRIORIDADE:
-            return 9 * 60
-        elif categoria in ('ATENDER', 'ATENDER - TAREFA'):
-            return inc.prazo
-        elif categoria == 'CORRIGIR':
-            return 135 * 60
-        elif categoria == 'ORIENTAR':
-            return 27 * 60
-        else:
-            assert 1 == 2 # should not happen
+            mesa_atual = inc.mesa_atual
+        return self.incsrv.calcular_prazo(inc, mesa_atual)
     
     def build_mesa_mapping(self, mesa):
         return {
@@ -119,4 +103,99 @@ class N4SapKpi(models.Kpi):
         summary[ 'VALOR'        ].append(kpi)
         summary[ 'SLA'          ].append(self.SLA)
         summary[ 'OBS'          ].append(obs)
-        
+
+class IncidentService(object): 
+    
+    SLA_PESO35              =   9 * 60
+    SLA_PESO30_ORIENTAR     =   9 * 60
+    SLA_PESO30_CORRIGIR     =  72 * 60
+    SLA_PESO30_ATENDER      =  36 * 60
+    SLA_ORIENTAR            =  40 * 60
+    SLA_CORRIGIR            = 135 * 60
+    SLA_ORIENTAR            =  27 * 60
+    SLA_ATENDER_SIMPLES     =  45 * 60
+    SLA_ATENDER_MEDIO       =  90 * 60
+    SLA_ATENDER_COMPLEXO    = 180 * 60
+    SLAS_ATENDER            = set([45 * 60, 90 * 60, 180 * 60])
+    SLA_ATENDER_DEFAULT     = 45 * 60
+    MESA_PRIORIDADE         = 'N4-SAP-SUSTENTACAO-PRIORIDADE'
+    MESA_ESCALADOS          = 'N4-SAP-SUSTENTACAO-ESCALADOS'
+    MESAS_NAO_PRIORITARIAS  = [
+        'N4-SAP-SUSTENTACAO-ABAST_GE'
+    ,   'N4-SAP-SUSTENTACAO-APOIO_OPERACAO'
+    ,   'N4-SAP-SUSTENTACAO-CORPORATIVO'
+    ,   'N4-SAP-SUSTENTACAO-FINANCAS' 
+    ,   'N4-SAP-SUSTENTACAO-GRC'
+    ,   'N4-SAP-SUSTENTACAO-PORTAL'
+    ,   'N4-SAP-SUSTENTACAO-SERVICOS'
+    ]
+    MESAS_CONTRATO          = [
+        'N4-SAP-SUSTENTACAO-ABAST_GE'
+    ,   'N4-SAP-SUSTENTACAO-APOIO_OPERACAO'
+    ,   'N4-SAP-SUSTENTACAO-CORPORATIVO'
+    ,   'N4-SAP-SUSTENTACAO-FINANCAS' 
+    ,   'N4-SAP-SUSTENTACAO-GRC'
+    ,   'N4-SAP-SUSTENTACAO-PORTAL'
+    ,   'N4-SAP-SUSTENTACAO-SERVICOS'
+    ,   'N4-SAP-SUSTENTACAO-PRIORIDADE'    
+    ,   'N4-SAP-SUSTENTACAO-ESCALADOS'    
+    ]
+    
+    def __init__(self, strict_orientar=False):
+        self.override_categorias = {}
+        self.strict_orientar = strict_orientar
+
+    def set_override_categoria(self, id_chamado, categoria):
+        self.override_categorias[ id_chamado ] = categoria
+    
+    def unset_override_categoria(self, id_chamado):
+        del self.override_categorias[ id_chamado ]        
+    
+    def categorizar(self, inc):
+        if inc.id_chamado in self.override_categorias:
+            return self.override_categorias[ inc.id_chamado ]
+        elif inc.id_chamado.startswith('T'):
+            return 'ATENDER - TAREFA'
+        elif inc.id_chamado.startswith('S'):
+            return 'ATENDER'
+        elif 'CORRIGIR' in inc.categoria.upper():
+            return 'CORRIGIR'
+        else:
+            if not self.strict_orientar:
+                return 'ORIENTAR'
+            elif self.strict_orientar and 'ORIENTAR' in inc.categoria.upper():
+                return 'ORIENTAR'
+            else:
+                return None
+    
+    def calcular_prazo(self, inc, mesa_atual):
+        assert mesa_atual is not None
+        assert mesa_atual in self.MESAS_CONTRATO
+        categoria = self.categorizar(inc)
+        assert categoria is not None
+        if mesa_atual == self.MESA_PRIORIDADE:
+            return self.SLA_PESO35
+        elif mesa_atual == self.MESA_ESCALADOS:
+            if categoria in ('ATENDER', 'ATENDER - TAREFA'):
+                return self.SLA_PESO30_ATENDER
+            elif categoria == 'CORRIGIR':
+                return self.SLA_PESO30_CORRIGIR
+            elif categoria == 'ORIENTAR':
+                return self.SLA_PESO30_ORIENTAR
+            assert 1 == 2 # should not happen
+        elif categoria in ('ATENDER', 'ATENDER - TAREFA'):
+            return inc.prazo if inc.prazo in self.SLAS_ATENDER else self.SLA_ATENDER_DEFAULT
+        elif categoria == 'CORRIGIR':
+            return 135 * 60
+        elif categoria == 'ORIENTAR':
+            return 27 * 60
+        assert 1 == 2 # should not happen
+    
+    def calc_duration_mesas(self, inc, mesas):
+        assert mesas is not None and len(mesas) > 0
+        return inc.calc_duration_mesas(mesas)
+    
+    def calc_pendencia_mesas(self, inc, mesas):
+        assert mesas is not None and len(mesas) > 0
+        return inc.calc_pendencia_mesas(mesas)
+    
--- pyclick/n4sap/tests/test_models.py
+++ pyclick/n4sap/tests/test_models.py
@@ -1,6 +1,7 @@
 from pyclick.models import *
 from pyclick.n4sap.models import *
 import unittest
+import unittest.mock as mock
 
 class TestN4SapKpi(unittest.TestCase):
     
@@ -32,3 +33,143 @@ class TestN4SapKpi(unittest.TestCase):
         categoria = self.n4.categorizar(inc)
         self.assertEqual("ATENDER", categoria)
 
+class TestIncidentService(unittest.TestCase):
+
+    def setUp(self):
+        self.incserv = IncidentService()
+        self.inc_orientar = Incidente(id_chamado='AAAAAA', chamado_pai=None, categoria='ORIENTAR', oferta='Dúvida sobre o serviço', prazo=1620)
+        self.inc_corrigir = Incidente(id_chamado='BBBBBB', chamado_pai=None, categoria='CORRIGIR', oferta='Suporte ao serviço de SAP', prazo=540)
+        self.inc_tarefa = Incidente(id_chamado='TCCCCC', chamado_pai='SDDDDD', categoria='Execução', oferta='HR / PY - Realização de Memória de Cálculo', prazo=45*60)
+        self.inc_sreq = Incidente(id_chamado='SDDDDD', chamado_pai=None, categoria='Atender', oferta='HR / PY - Realização de Memória de Cálculo', prazo=45*60)
+        self.inc_pseudo_orientar = Incidente(id_chamado='EEEEEE', chamado_pai=None, categoria='Outra Falha', oferta='Suporte aos serviços', prazo=540)
+    
+    def tearDown(self):
+        pass
+    
+    def test_it_categorizes_an_incident(self):
+        categoria = self.incserv.categorizar(self.inc_orientar)
+        self.assertEqual('ORIENTAR', categoria)
+        categoria = self.incserv.categorizar(self.inc_corrigir)
+        self.assertEqual('CORRIGIR', categoria)
+        categoria = self.incserv.categorizar(self.inc_tarefa)
+        self.assertEqual('ATENDER - TAREFA', categoria)
+        categoria = self.incserv.categorizar(self.inc_sreq)
+        self.assertEqual('ATENDER', categoria)
+        categoria = self.incserv.categorizar(self.inc_pseudo_orientar)
+        self.assertEqual('ORIENTAR', categoria)
+        self.incserv.strict_orientar = True
+        categoria = self.incserv.categorizar(self.inc_pseudo_orientar)
+        self.assertEqual(None, categoria)
+    
+    def test_it_overrides_categorization(self):
+        categoria = self.incserv.categorizar(self.inc_orientar)
+        self.assertEqual('ORIENTAR', categoria)
+        
+        self.incserv.set_override_categoria(self.inc_orientar.id_chamado, 'CORRIGIR')
+        categoria = self.incserv.categorizar(self.inc_orientar)
+        self.assertEqual('CORRIGIR', categoria)
+        
+        self.incserv.unset_override_categoria(self.inc_orientar.id_chamado)
+        categoria = self.incserv.categorizar(self.inc_orientar)
+        self.assertEqual('ORIENTAR', categoria)
+        
+    def test_it_computes_inc_duration(self):
+        acao1 = Acao(id_acao=1, acao_nome='Atribuição interna', pendencia='N', mesa_atual='MESA-0', data_acao='2020-01-01 09:30:00', data_fim_acao='2020-01-01 10:00:00', duracao_m=30)
+        acao2 = Acao(id_acao=2, acao_nome='Atribuição interna', pendencia='N', mesa_atual='MESA-1', data_acao='2020-01-01 10:00:00', data_fim_acao='2020-01-01 10:30:00', duracao_m=30)
+        
+        with self.assertRaises(AssertionError):
+            self.incserv.calc_duration_mesas(self.inc_orientar, None)
+        with self.assertRaises(AssertionError):
+            self.incserv.calc_duration_mesas(self.inc_orientar, [])
+        
+        self.inc_orientar.add_acao(acao1)
+        self.inc_orientar.add_acao(acao2)
+        duration_m = self.incserv.calc_duration_mesas(self.inc_orientar, ['MESA-1'])
+        self.assertEqual(duration_m, 30)
+
+    def test_it_computes_inc_pending_time(self):
+        acao1 = Acao(id_acao=1, acao_nome='Atribuição interna', pendencia='S', mesa_atual='MESA-1', data_acao='2020-01-01 09:30:00', data_fim_acao='2020-01-01 10:00:00', duracao_m=30)
+        acao2 = Acao(id_acao=2, acao_nome='Atribuição interna', pendencia='N', mesa_atual='MESA-1', data_acao='2020-01-01 10:00:00', data_fim_acao='2020-01-01 10:25:00', duracao_m=30)
+        
+        with self.assertRaises(AssertionError):
+            self.incserv.calc_pendencia_mesas(self.inc_orientar, None)
+        with self.assertRaises(AssertionError):
+            self.incserv.calc_pendencia_mesas(self.inc_orientar, [])
+        
+        self.inc_orientar.add_acao(acao1)
+        self.inc_orientar.add_acao(acao2)
+        duration_m = self.incserv.calc_pendencia_mesas(self.inc_orientar, ['MESA-1'])
+        self.assertEqual(duration_m, 30)
+    
+    def test_it_computes_sla_for_an_inc(self):
+        with self.assertRaises(AssertionError):
+            sla = self.incserv.calcular_prazo(self.inc_orientar, mesa_atual=None)
+        with self.assertRaises(AssertionError):
+            sla = self.incserv.calcular_prazo(self.inc_orientar, mesa_atual='N4-MESA-FORA-DO-CONTRATO')
+        # orientar
+        sla = self.incserv.calcular_prazo(self.inc_orientar, mesa_atual='N4-SAP-SUSTENTACAO-SERVICOS')
+        self.assertEqual(27*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_orientar, mesa_atual='N4-SAP-SUSTENTACAO-PRIORIDADE')
+        self.assertEqual(9*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_orientar, mesa_atual='N4-SAP-SUSTENTACAO-ESCALADOS')
+        self.assertEqual(9*60, sla)
+        
+        # corrigir
+        sla = self.incserv.calcular_prazo(self.inc_corrigir, mesa_atual='N4-SAP-SUSTENTACAO-SERVICOS')
+        self.assertEqual(135*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_corrigir, mesa_atual='N4-SAP-SUSTENTACAO-PRIORIDADE')
+        self.assertEqual(9*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_corrigir, mesa_atual='N4-SAP-SUSTENTACAO-ESCALADOS')
+        self.assertEqual(72*60, sla)
+        
+        # atender - tarefa
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-SERVICOS')
+        self.assertEqual(45*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-PRIORIDADE')
+        self.assertEqual(9*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-ESCALADOS')
+        self.assertEqual(36*60, sla)
+        
+        # atender - solicitação de serviço
+        sla = self.incserv.calcular_prazo(self.inc_sreq, mesa_atual='N4-SAP-SUSTENTACAO-SERVICOS')
+        self.assertEqual(45*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_sreq, mesa_atual='N4-SAP-SUSTENTACAO-PRIORIDADE')
+        self.assertEqual(9*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_sreq, mesa_atual='N4-SAP-SUSTENTACAO-ESCALADOS')
+        self.assertEqual(36*60, sla)
+        
+        # atender - tarefa - complexidade simples
+        self.inc_tarefa.prazo = IncidentService.SLA_ATENDER_SIMPLES
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-SERVICOS')
+        self.assertEqual(IncidentService.SLA_ATENDER_SIMPLES, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-PRIORIDADE')
+        self.assertEqual(9*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-ESCALADOS')
+        self.assertEqual(36*60, sla)
+
+        # atender - tarefa - complexidade média
+        self.inc_tarefa.prazo = IncidentService.SLA_ATENDER_MEDIO
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-SERVICOS')
+        self.assertEqual(IncidentService.SLA_ATENDER_MEDIO, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-PRIORIDADE')
+        self.assertEqual(9*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-ESCALADOS')
+        self.assertEqual(36*60, sla)
+
+        # atender - tarefa - complexidade alta
+        self.inc_tarefa.prazo = IncidentService.SLA_ATENDER_COMPLEXO
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-SERVICOS')
+        self.assertEqual(IncidentService.SLA_ATENDER_COMPLEXO, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-PRIORIDADE')
+        self.assertEqual(9*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-ESCALADOS')
+        self.assertEqual(36*60, sla)
+
+        # atender - tarefa - complexidade não padronizada
+        self.inc_tarefa.prazo = IncidentService.SLA_ATENDER_COMPLEXO + 1
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-SERVICOS')
+        self.assertEqual(IncidentService.SLA_ATENDER_SIMPLES, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-PRIORIDADE')
+        self.assertEqual(9*60, sla)
+        sla = self.incserv.calcular_prazo(self.inc_tarefa, mesa_atual='N4-SAP-SUSTENTACAO-ESCALADOS')
+        self.assertEqual(36*60, sla)
