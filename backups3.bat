71193451e1ced4254fb1ddfb41190e2028b59bb1
Daniel Lemos Itaboraí <itaborai83@yahoo.com.br>
2020-11-27 16:16:55 -0300

    fix console termination on s3 backup script

--- backups3.bat
+++ backups3.bat
@@ -1,2 +1,2 @@
-aws s3 sync DADOS\STAGING s3://itaborai83-pyclick-bkp-staging
-aws s3 sync DADOS\IMPORT s3://itaborai83-pyclick-bkp-import --exclude *.db
+cmd /c aws s3 sync DADOS\STAGING s3://itaborai83-pyclick-bkp-staging
+cmd /c aws s3 sync DADOS\IMPORT s3://itaborai83-pyclick-bkp-import --exclude *.db
